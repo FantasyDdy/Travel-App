5eaf6c16bc66634d5b93a34bf30e668b48a31702
FantasyDdy <daidongyaxi@163.com>
2020-11-01 22:17:29 +0800

    修复--对全局事件的解绑

--- src/components/detail/Header.vue
+++ src/components/detail/Header.vue
@@ -49,13 +49,20 @@ export default {
             }else{
                 this.showAbs = true;
             }
+            console.log('scroll')
         }
     },
     // 因为Detail组件也是通过被<keep-alive>组件包裹的<router-view>组件渲染出来的，所以Detail组件的页面一被渲染就会执行activated钩子
     // 当组件在 <keep-alive> 内被切换，它的 activated 和 deactivated 这两个生命周期钩子函数将会被对应执行。
     activated(){
         // 每一次渲染都会监听scroll事件
-        window.addEventListener('scroll',this.handleScroll)
+        window.addEventListener('scroll',this.handleScroll);
+    },
+    // 对全局事件的解绑
+    // 因为在activated钩子中绑定的是一个window全局对象，当执行过这条语句后，其他页面滚动也会触发此监听语句。
+    deactivated(){  //被 keep-alive 缓存的组件停用时调用改钩子
+        // 当keep-alive停用此组件时，解绑全局事件
+        window.removeEventListener('scroll',this.handleScroll);
     }
 }
 </script>
